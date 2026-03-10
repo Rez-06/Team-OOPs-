0da0d7dfdc640ab14b1cb339682608a6f59ae839
jvvhid <jahidul2k04@gmail.com>
2026-03-10 10:11:51 +0600

    Implement  AuthorizedPerson

---
+++ AuthorizedPerson.cpp
@@ -0,0 +1,37 @@
+#include "AUTHORIZED_PERSON_H"
+#include <iostream>
+using namespace std;
+
+class Admin : public AuthorizedPerson {
+//admin
+public:
+    Admin(string Name, int Id, string Email)
+        : AuthorizedPerson(Name, Id, Email) {}
+
+    void Identity() const override {
+        cout << "Role: Admin" << endl;
+        cout << "Name: " << name << endl;
+        cout << "ID: " << id << endl;
+        cout << "Email: " << eMail << endl;
+    }
+
+    string post() const override {
+        return "Admin";
+    }
+};
+//customer
+class Customer : public AuthorizedPerson {
+public:
+    Customer(string Name, int Id, string Email)
+        : AuthorizedPerson(Name, Id, Email) {}
+        
+    void Identity() const override {
+        cout << "Role: Customer" << endl;
+        cout << "Name: " << name << endl;
+        cout << "ID: " << id << endl;
+        cout << "Email: " << eMail << endl;
+    }
+    string post() const override {
+        return "Customer";
+    }
+};
