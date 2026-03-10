93f21de845b1cab75684f49c71235079e0373651
jvvhid <jahidul2k04@gmail.com>
2026-03-10 10:54:17 +0600

    Implement customer billing and game reservation methods

---
+++ Customer.cpp
@@ -0,0 +1,62 @@
+#include "customer.h"
+#include <iostream>
+using namespace std;
+
+void customer::Display_bill()
+{
+    total_bill = food_bill + game_bill;
+
+    cout << "\n===== BILL SUMMARY =====\n";
+    cout << "Food Bill: " << food_bill << endl;
+    cout << "Game Bill: " << game_bill << endl;
+    cout << "Total Bill: " << total_bill << endl;
+}
+void customer::display_food_menu()
+{
+    Food_menu menu;
+    menu.seeMenu();
+}
+void customer::display_game_collection()
+{
+    GameCatalogue catalogue;
+    catalogue.viewAllGames();
+}
+void customer::order_food()
+{
+    int id;
+    double price;
+
+    Food_menu menu;
+
+    cout << "\nSelect food item ID: ";
+    cin >> id;
+
+    cout << "Enter price of selected item: ";
+    cin >> price;
+
+    food_bill += price;
+
+    cout << "Food added to bill.\n";
+}
+
+void customer::reserve_game_zone()
+{
+    Rental rental;
+
+    string timeSlot;
+    int controllers;
+    int minutes;
+
+    cout << "\nEnter time slot: ";
+    cin >> timeSlot;
+
+    cout << "Controllers required: ";
+    cin >> controllers;
+
+    cout << "Duration (minutes): ";
+    cin >> minutes;
+
+    game_bill += rental.calculatePrice(controllers, minutes);
+
+    cout << "Game zone reserved.\n";
+}
